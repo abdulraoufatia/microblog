ccc522f13ed8857d874c8662cfccaa76305a5d61
abdulraoufatia <abdulraoufatia@outlook.com>
2022-03-10 00:06:21 +0000

    L-66: Basic Templating

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'username': 'Abdulraouf Atia'}
+    return render_template('index.html', title = 'Home', user=user)
