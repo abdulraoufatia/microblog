af36d6881bc38b29b4064387a1f901ea39fa342b
Abdulraouf Atia <abdulraoufatia@outlook.com>
2022-08-18 11:19:39 +0100

    L-295: Login required

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
+from urllib import request
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user,login_user, logout_user
+from flask_login import current_user,login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
+from flask import request
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Abdulraouf Atia'}
     posts = [
@@ -29,10 +33,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Incorrect username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+           next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
